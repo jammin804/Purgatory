741db928c548cc501396f33bb2bf99f434c61ce2
jammin804 <jammin804@gmail.com>
2020-10-26 14:39:10 -0400

    Remainder of the first commit.

--- GameTemplate/Powerup.h
+++ GameTemplate/Powerup.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include "Framework/GameObject.h"
+
+class BoxCollisionComponent;
+
+class Powerup : public GameObject
+{
+protected:
+	virtual void OnInit() override;
+	virtual void OnPostInit() override;
+	virtual void OnUpdate(float DeltaTime) override;
+
+public:
+	void SetMovementSpeed(float NewMovementSpeed) { MovementSpeed = NewMovementSpeed; }
+	float GetMovementSpeed() const { return MovementSpeed; }
+	void SetMovementDirection(float DirX, float DirY) { MovementDirectionX = DirX; MovementDirectionY = DirY; }
+	float GetMovementDirectionX() const { return MovementDirectionX; }
+	float GetMovementDirectionY() const { return MovementDirectionY; }
+	const BoxCollisionComponent* GetCollision() const { return Collision; }
+private:
+	float RotationSpeed = 1.0f;
+	class ImageComponent* RockImage = nullptr;
+	BoxCollisionComponent* Collision = nullptr;
+	float MovementSpeed = 100.0f;
+	float MovementDirectionX = 0.0f;
+	float MovementDirectionY = 0.0f;
+};
