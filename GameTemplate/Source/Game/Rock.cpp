ae5bbd54a992e0267fa1590c801e1ac82eeb2835
jammin804 <jammin804@gmail.com>
2020-11-20 20:01:23 -0500

    Started the enemy movment function, but i can not get the timer to trigger the conditional statment.

--- GameTemplate/Source/Game/Rock.cpp
+++ GameTemplate/Source/Game/Rock.cpp
@@ -24,11 +24,43 @@ void Rock::OnPostInit()
 
 void Rock::OnUpdate(float DeltaTime)
 {
-    //steal for powerup movement
-	SetRotation(GetRotation() + RotationSpeed * DeltaTime);
+	bNeedsSwitch = true;
+	MoveTimer += DeltaTime;
+	if (MoveTimer > MaxMoveTime)
+	{
+		Switch();
+	}
+
+    //steal for power up movement
+	//SetRotation(GetRotation() + RotationSpeed * DeltaTime); Removing rotation
     
-    SetPosition(GetPositionX() + (MovementDirectionX * MovementSpeed * DeltaTime),
-        GetPositionY() + (MovementDirectionY * MovementSpeed * DeltaTime));
+    /*SetPosition(GetPositionX() + (MovementDirectionX * MovementSpeed * DeltaTime),
+        GetPositionY() + (MovementDirectionY * MovementSpeed * DeltaTime));*/
+
+	/*for (int i=0; i<2; i++) //tried to make a test to see if i can make a mini timer
+	{
+		if (i == 2) 
+		{
+			bNeedsSwitch = false;
+			i = 0;
+		}
+	}*/
+
+	if (bNeedsSwitch)
+	{
+		SetPosition(GetPositionX(), GetPositionY() + (MovementDirectionY * MovementSpeed * DeltaTime));
+	}
+	else 
+	{
+		SetPosition(650.0f, 100.0f);
+	}
+
+	/*if (MoveTimer > 30.0f)
+	{
+		bNeedsSwitch = false;
+		MoveTimer = 0.0f;
+	}*/
+
 	// Setting timers to switch direction. A bool in order to switch directions
     LifeTimer += DeltaTime;
     if (LifeTimer > MaxLifeTime)
@@ -59,6 +91,11 @@ void Rock::Split()
     float CurrentMovementDirectionX = MovementDirectionX;
     MovementDirectionX = MovementDirectionY;
     MovementDirectionY = CurrentMovementDirectionX;
+}
+
+void Rock::Switch()
+{
+	bNeedsSwitch = true;
 }
 
 void Rock::RequestSplit()
--- GameTemplate/Source/Game/Rock.h
+++ GameTemplate/Source/Game/Rock.h
@@ -23,6 +23,8 @@ public:
     void SetMovementDirection(float DirX, float DirY) { MovementDirectionX = DirX; MovementDirectionY = DirY; }
     float GetMovementDirectionX() const { return MovementDirectionX; }
     float GetMovementDirectionY() const { return MovementDirectionY; }
+	void Switch(); // Function to switch when function is called
+	bool SwitchDirection() const { return bNeedsSwitch; } // 
     const BoxCollisionComponent* GetCollision() const { return Collision; }
 private:
     float RotationSpeed = 1.0f;
@@ -30,11 +32,14 @@ private:
     BoxCollisionComponent* Collision = nullptr;
     float LifeTimer = 0.0f;
     float MaxLifeTime = 30.0f;
+	float MoveTimer = 0.0f; // when timer runs out switch
+	float MaxMoveTime = 30.0f;
     bool bNeedsSplit = false;
     int SplitsLeft = 2;
     float MovementSpeed = 100.0f;
     float MovementDirectionX = 0.0f;
     float MovementDirectionY = 0.0f;
+	bool bNeedsSwitch = false; // bool for switching
 public:
     void RequestSplit();
 };
