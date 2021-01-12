97ad5b82ff01896b83d6e4e38e446aa5fd869558
jammin804 <jammin804@gmail.com>
2021-01-11 21:56:15 -0500

    Added Trigonometry in the cross class in attempts to make it rotate in a circle when the player fires.

--- GameTemplate/Source/Game/Cross.cpp
+++ GameTemplate/Source/Game/Cross.cpp
@@ -39,11 +39,26 @@ void Cross::OnUpdate(float DeltaTime)
     SetPosition(GetPositionX() + DirectionX, GetPositionY() - DirectionY);
 	SetRotation(GetRotation() + CrossRotationSpeed * DeltaTime);
 
+	// Moving Bullet in a circle formation
+	/*float PosX = 0.0f;
+	float PosY = 0.0f;
+	float OriginX = GetPositionX();
+	float OriginY = GetPositionY();
+	float Radius = 10.0f;
+	int Angle = 0;
+	float PI = 3.14;
+
+	PosX = OriginX + cos(Angle)*Radius;
+	PosY = OriginY + sin(Angle)*Radius;
+
+	Angle += 2 * PI / 180;*/
+
     LifeTimer += DeltaTime;
     if (LifeTimer > MaxLifeTime)
     {
         RequestDestroy();
     }
+
 }
 
 void Cross::SetInitialDirection(float x, float y)
