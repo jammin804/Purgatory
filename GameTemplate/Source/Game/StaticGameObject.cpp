e620256d8c2b0889dfffac895cbe3965df701ada
jammin804 <jammin804@gmail.com>
2021-02-15 18:48:39 -0500

    More edits to the static game object

--- GameTemplate/Source/Game/StaticGameObject.cpp
+++ GameTemplate/Source/Game/StaticGameObject.cpp
@@ -4,12 +4,20 @@
 
 void StaticGameObject::OnInit()
 {
-
+	StaticObjectImage = GameComponent::CreateInstance<ImageComponent>(this);
+	Collision = GameComponent::CreateInstance<BoxCollisionComponent>(this);
 }
 
 void StaticGameObject::OnPostInit()
 {
+	std::string StaticObjectImagePath = "";
+	float StaticImageScale = 1.0f;
 
+	if (StaticObjectImage)
+	{
+		StaticObjectImage->LoadImage(StaticObjectImage);
+		StaticObjectImage->SetScale(StaticImageScale);
+	}
 }
 
 void StaticGameObject::OnUpdate(float DeltaTime)
